0b9f989e5580f145bd53a2245e96062520517550
hugowellens <114393623+hugowellens@users.noreply.github.com>
2022-09-26 12:10:14 +0200

    Add files via upload

--- app2.py
+++ app2.py
@@ -7,7 +7,7 @@
 # pip install streamlit
 
 
-# In[4]:
+# In[2]:
 
 
 import streamlit as st
@@ -15,7 +15,7 @@ import pandas as pd
 import numpy as np
 
 
-# In[5]:
+# In[3]:
 
 
 HairEye = pd.read_csv("HairEyeColor.csv")
@@ -27,20 +27,27 @@ HairEye = pd.read_csv("HairEyeColor.csv")
 st.title("Hair Eye Colour Database")
 
 
-# In[ ]:
+# In[5]:
 
 
 InputHair = st.sidebar.selectbox("Select Hair Colour", ("Brown", "Black", "Blond", "Red"))
 
 
-# In[ ]:
+# In[8]:
 
 
 HairEyeselect = HairEye[HairEye["Hair"] == InputHair]
+HairEyeselect
+
+
+# In[9]:
+
+
+st.dataframe(HairEyeselect)
 
 
 # In[ ]:
 
 
-st.dataframe(HairEyeSelect)
+
 
